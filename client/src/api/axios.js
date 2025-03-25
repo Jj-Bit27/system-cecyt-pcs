87fe0b7d5cfc9498c34acf0605147fb4fee1f94c
Juan Jose <22308051220637@cbtis122.edu.mx>
2025-03-25 12:21:55 -0600

    fixxed archive

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 /* La url del servidor */
-const API_URL = `${process.env.API_URL}`;
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
 console.log(API_URL);
-console.log("API URL:", process.env.API_URL);
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 
 /* Crear una instancia de axios */
 const instance = axios.create({
