09bb3fb1479b8c5dd67227f2c73bcf1ced3e8652
Juan Jose <22308051220637@cbtis122.edu.mx>
2025-03-25 12:10:44 -0600

    fix url api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /* La url del servidor */
-const API_URL = import.meta.env.API_URL;
+const API_URL = `${import.meta.env.API_URL}/api`;
 
 /* Crear una instancia de axios */
 const instance = axios.create({
