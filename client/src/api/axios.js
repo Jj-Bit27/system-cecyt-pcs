84739962b2a995b76721fce0a2fca119d2324428
Juan Jose <22308051220637@cbtis122.edu.mx>
2025-03-25 12:29:09 -0600

    fixxed api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 /* La url del servidor */
-const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
+const API_URL = `https://system-cecyt-pcs-api.onrender.com/api`;
 console.log(API_URL);
-console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 
 /* Crear una instancia de axios */
 const instance = axios.create({
